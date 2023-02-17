868378bebcd60e076c9d11ede171b18ef99dcf66
Polina Kolesnikova <polina@example.com>
2023-02-17 22:10:20 +0400

    решение в лоб

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+
+numbers = [1, 2, 3, 4, 5, 6]
+for a in numbers:
+    for b in numbers:
+        for c in numbers:
+            for d in numbers:
+                for e in numbers:
+                    for f in numbers:
+                        if (a*10+b)**c == (d*10+e)**f:
+                            list_numbers = [a,b,c,d,e,f]
+                            if len(set(list_numbers)) == 6:
+                                print(a, b, c, d, e, f)
